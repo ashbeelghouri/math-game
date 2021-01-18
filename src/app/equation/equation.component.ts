0f472c1810c4912a71424c4e4e9e06f79a38a4cd
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2021-01-18 05:35:26 +0500

    time taken simple solution

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { delay, filter } from 'rxjs/operators';
 import { CustomValidators } from '../custom-validators';
 @Component({
   selector: 'app-equation',
@@ -8,25 +9,60 @@ import { CustomValidators } from '../custom-validators';
 })
 export class EquationComponent implements OnInit {
 
+  secondsPerSolution = 0;
+
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
-    answer: new FormControl('')  
+    answer: new FormControl('')
   }, [CustomValidators.addition('answer', 'a', 'b')]);
 
   constructor() { }
 
-  get a(){
+  get a() {
     return this.mathForm.value.a;
   }
-  get b(){
+  get b() {
     return this.mathForm.value.b;
   }
 
-  ngOnInit(): void {
+  reset() {
+    // to update all -- not works on singular
+    // this.mathForm.setValue({
+    //   a: this.randomNumber(),
+    //   b: this.randomNumber(),
+    //   answer: ''
+    // });
+    // if we need to update singular or some multiple values
+    this.mathForm.patchValue({
+      a: this.randomNumber(),
+      b: this.randomNumber(),
+      answer: ''
+    });
+    // this.mathForm.controls.a.setValue(this.randomNumber());      
+    // this.mathForm.controls.b.setValue(this.randomNumber());      
+    // this.mathForm.controls.answer.setValue('');
   }
 
-  randomNumber(){
+  ngOnInit(): void {
+
+    const startTime = new Date();
+
+    let numberSolved = 0;
+
+    this.mathForm.statusChanges.pipe(
+      filter(value => value == 'VALID'),
+      delay(100)).subscribe(value => {
+        numberSolved++;
+        this.secondsPerSolution = (
+          new Date().getTime() - startTime.getTime()
+        ) / numberSolved / 1000;
+        this.reset();
+
+      })
+  }
+
+  randomNumber() {
     return Math.floor(Math.random() * 10);
   }
 
